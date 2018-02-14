bbe3e09b779a2433eb851f0e4e2120d95c7b83a1
Jan Procházka <jan.prochazka@ubnt.com>
2018-02-14 15:44:43 +0100

    Directory change

--- plugins/fio_cz/src/Service/OptionsManager.php
+++ plugins/fio_cz/src/Service/OptionsManager.php
@@ -52,11 +52,10 @@ class OptionsManager
 
     private function getDataFromJson(string $filename): array
     {
-        $decoded = json_decode(file_get_contents($filename), true);
-        if (\is_array($decoded)) {
-            return $decoded;
+        if (! file_exists($filename)) {
+            return [];
         }
 
-        return [];
+        return json_decode(file_get_contents($filename), true);
     }
 }
