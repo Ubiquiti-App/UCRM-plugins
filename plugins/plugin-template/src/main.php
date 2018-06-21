845a79432093316881394fa0d674b99d1fec4608
Jan Piskvor Martinec <github@piskvor.org>
2018-06-21 14:28:25 +0200

    update example to show current plugin's directory

--- plugins/plugin-template/src/main.php
+++ plugins/plugin-template/src/main.php
@@ -1,10 +1,14 @@
 <?php
 
-chdir(__DIR__);
+// __DIR__ means "directory of _this_ file, i.e. where main.php is"
+define('EXAMPLE_PLUGIN_BASE_DIRECTORY', __DIR__);
+// now we are sure that we're currently running in this directory
+chdir(EXAMPLE_PLUGIN_BASE_DIRECTORY);
 
 require_once 'src/Logger.php';
 
 (function () {
     $logger = new \SampleLogger\Logger();
+    $logger->log('Plugin\'s directory: ' . EXAMPLE_PLUGIN_BASE_DIRECTORY);
     $logger->log('Finished');
 })();
