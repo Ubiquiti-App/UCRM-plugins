99a370e8aa088df1366469c350b315aedfffc551
Jáchym Toušek <enumag@gmail.com>
2018-10-05 15:52:16 +0200

    Update plugin

--- examples/example-invoice-csv-export/src/public.php
+++ examples/example-invoice-csv-export/src/public.php
@@ -1,5 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 chdir(__DIR__);
 
+require __DIR__ . '/src/functions.php';
 
+$config = loadConfig(__DIR__ . '/ucrm.json');
+
+$user = retrieveCurrentUser($config['ucrmPublicUrl']);
+
+echo 'Api key is: ' . $config['pluginAppKey'];
+
+var_export($user);
