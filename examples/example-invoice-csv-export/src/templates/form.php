f91bf1580a43ac2fe98f2914adaf73b1e00c5248
Jáchym Toušek <enumag@gmail.com>
2018-10-10 12:13:05 +0200

    Update plugin

--- examples/example-invoice-csv-export/src/templates/form.php
+++ examples/example-invoice-csv-export/src/templates/form.php
@@ -1,16 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Invoice export</title>
+</head>
+<body>
+<h1>Export invoices</h1>
 <form>
-    <table>
-        <tr>
-            <td>Since:</td>
-            <td><input type="date" name="since"></td>
-        </tr>
-        <tr>
-            <td>Until:</td>
-            <td><input type="date" name="until"></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td style="text-align: right"><button type="submit">Export</button></td>
-        </tr>
-    </table>
+	<table>
+		<tr>
+			<td>Since:</td>
+			<td><input type="date" name="since"></td>
+		</tr>
+		<tr>
+			<td>Until:</td>
+			<td><input type="date" name="until"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td style="text-align: right"><button type="submit">Export</button></td>
+		</tr>
+	</table>
 </form>
+</body>
+</html>
