ba5891351de61b5fc8ed7e15786ab7fd37b46dcc
Jáchym Toušek <enumag@gmail.com>
2018-10-12 12:34:01 +0200

    Add plugin skeleton

---
+++ examples/example-revenue-report/src/classes/Data/UcrmData.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data;
+
+class UcrmData
+{
+    /**
+     * @var string
+     */
+    public $pluginAppKey;
+
+    /**
+     * @var string
+     */
+    public $ucrmPublicUrl;
+}
