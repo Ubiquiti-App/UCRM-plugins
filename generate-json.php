f43cdaf96c4838bba6ff7eed38952d1710356cc0
Jáchym Toušek <enumag@gmail.com>
2018-10-31 13:34:32 +0100

    Add zipUrl to plugins.json

--- generate-json.php
+++ generate-json.php
@@ -1,22 +1,25 @@
 <?php
 
-function findPluginDirectories(): Traversable
-{
-    return new CallbackFilterIterator(
-        new DirectoryIterator(__DIR__ . '/plugins'),
-        function (DirectoryIterator $fileInfo) {
-            return $fileInfo->isDir() && ! $fileInfo->isDot();
-        }
-    );
-}
+$pluginDirectories = new CallbackFilterIterator(
+    new DirectoryIterator(__DIR__ . '/plugins'),
+    function (DirectoryIterator $fileInfo) {
+        return $fileInfo->isDir() && ! $fileInfo->isDot();
+    }
+);
 
 $plugins = [];
-foreach (findPluginDirectories() as $directory) {
+foreach ($pluginDirectories as $directory) {
     $file = $directory->getPathname() . '/src/manifest.json';
 
     $manifest = json_decode(file_get_contents($file), true);
+    $plugin = $manifest['information'];
+    $plugin['zipUrl'] = sprintf(
+        'https://github.com/Ubiquiti-App/UCRM-plugins/raw/master/plugins/%s/%s.zip',
+        $plugin['name'],
+        $plugin['name']
+    );
 
-    $plugins[] = $manifest['information'];
+    $plugins[] = $plugin;
 }
 
 echo json_encode(['plugins' => array_values(array_filter($plugins))], JSON_PRETTY_PRINT);
