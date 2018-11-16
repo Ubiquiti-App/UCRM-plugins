74fc29ee34a3cfee259ec571ef8edd87063d7458
Jan Piskvor Martinec <github@piskvor.org>
2018-11-16 14:05:47 +0100

    show progress+errors when generating plugins.json

--- generate-json.php
+++ generate-json.php
@@ -11,17 +11,23 @@ $plugins = [];
 foreach ($pluginDirectories as $directory) {
     $file = $directory->getPathname() . '/src/manifest.json';
 
-    $manifest = json_decode(file_get_contents($file), true);
+    $manifest = json_decode(file_get_contents($file), true, 50);
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        fwrite(STDERR, sprintf('Skipped file "%s": %s' . PHP_EOL, $file, json_last_error_msg()));
+        continue;
+    }
     $plugin = $manifest['information'];
     $plugin['zipUrl'] = sprintf(
         'https://github.com/Ubiquiti-App/UCRM-plugins/raw/master/plugins/%s/%s.zip',
         $plugin['name'],
         $plugin['name']
     );
+    fwrite(STDERR, sprintf("\t%s\t%s\t\t%s\n", $plugin['version'], $plugin['name'], $plugin['url']));
 
     $plugins[$plugin['name']] = $plugin;
 }
 
 ksort($plugins);
 
+fwrite(STDERR, 'Plugins found: ' . count($plugins) . "\n");
 echo json_encode(['plugins' => array_values(array_filter($plugins))], JSON_PRETTY_PRINT) . PHP_EOL;
