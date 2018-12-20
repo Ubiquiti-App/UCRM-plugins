cabb0a0874fceec8b4300ba82eaaa603d396f7cd
Brandon White <mpcomputertech1@gmail.com>
2018-12-20 16:17:23 -0700

    interpreter tests

--- plugins/ucrm-client-signup/src/includes/api-interpreter.php
+++ plugins/ucrm-client-signup/src/includes/api-interpreter.php
@@ -1,20 +1,9 @@
 <?php
-
 $payload = @file_get_contents("php://input");
 
-$pluginLogManager = new \Ubnt\UcrmPluginSdk\Service\PluginLogManager();
-$pluginLogManager->appendLog('This plugin just did something and wants to let you know about it.');
-
-# Requests should include frontendKey
-if (!empty($payload['frontendKey'])) {
-  
-
-  if (!empty($payload['apiGet'])) {
-    \USCP\Interpreter::get($payload['apiGet']['endpoint'], $payload['apiGet']['data']);
-  } elseif (!empty($payload['apiPost'])) {
-    \USCP\Interpreter::post($payload['apiPost']['endpoint'], $payload['apiPost']['data']);
-  }
-
-  # Exit public.php to prevent following HTML from rendering
+$interpreter = new \Ucsp\Interpreter();
+$result = $interpreter->run($payload);
+if ($result) {
+  echo $result;
   exit();
 }
