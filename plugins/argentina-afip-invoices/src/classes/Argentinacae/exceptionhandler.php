2a56843324ec2e6e14290644540526eefe9a8085
fgampel <franco@gampel.com.ar>
2019-02-01 17:59:48 -0300

    Public Release Argentina-AFIP-invoices

---
+++ plugins/argentina-afip-invoices/src/classes/Argentinacae/exceptionhandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Argentinacae;
+
+set_exception_handler('exception_handler');
+
+function exception_handler(Exception $e) 
+{
+  echo $e->getFile().':'.$e->getLine().'  ' . $e->getMessage();
+}
+
+?>
