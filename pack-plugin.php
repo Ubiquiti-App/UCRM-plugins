5e38c4d98000ad64e452bd6204225c82fa06342a
Jan Piskvor Martinec <github@piskvor.org>
2019-08-21 12:28:55 +0200

    if composer install complains, do not proceed with plugin packing

--- pack-plugin.php
+++ pack-plugin.php
@@ -28,7 +28,7 @@ chdir($directory);
 
 if (file_exists($directory . '/composer.json')) {
     shell_exec('composer validate --no-check-publish --no-interaction');
-    shell_exec('composer install --classmap-authoritative --no-dev --no-interaction --no-suggest');
+    system('composer install --classmap-authoritative --no-dev --no-interaction --no-suggest') or exit(1);
 }
 
 $zipFile = $directory . '/../' . $plugin . '.zip';
