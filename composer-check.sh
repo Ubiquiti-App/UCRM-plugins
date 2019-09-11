afa29139b04c0b4857fa45211a6bade4bcb500e2
Jan Piskvor Martinec <github@piskvor.org>
2019-09-11 15:23:08 +0200

    check composer state

---
+++ composer-check.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+#set -o xtrace
+
+cd "$(dirname "$0")"
+
+composer check
+HAS_WARNINGS="$(composer install --no-interaction --no-suggest --ansi |& tee /dev/stderr | grep -ci Warning || true)"
+if [[ "$HAS_WARNINGS" -gt 0 ]]; then
+  echo "composer check failed" > /dev/stderr
+fi
+exit "$HAS_WARNINGS"
