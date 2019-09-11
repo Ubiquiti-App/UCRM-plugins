c128cf98700efa2756dae43b437bbd3cf1a08be7
Jan Piskvor Martinec <github@piskvor.org>
2019-09-11 16:13:01 +0200

    fix spacing

--- php-cs-check.sh
+++ php-cs-check.sh
@@ -12,13 +12,13 @@ DIR_NAME="$(pwd)"
 PHP_CS_FIXER="${DIR_NAME}/vendor/bin/php-cs-fixer"
 PHP_CF_OPTIONS=(--diff "--diff-format=udiff" --no-interaction --ansi "--rules=@PHP73Migration")
 if [[ ! -x "$PHP_CS_FIXER" ]]; then
-	composer install --no-interaction --no-suggest --quiet
+  composer install --no-interaction --no-suggest --quiet
 fi
 
 ${PHP_CS_FIXER} fix --quiet "${PHP_CF_OPTIONS[@]}" --dry-run "${DIR_NAME}" ||
-	(
-		RESULT=$?
-		${PHP_CS_FIXER} fix "${PHP_CF_OPTIONS[@]}" --dry-run "${DIR_NAME}" ||
-			printf " %s" "Fix by running: " "${PHP_CS_FIXER}" fix "${PHP_CF_OPTIONS[@]}" "${DIR_NAME}" $'\n'
-		exit ${RESULT}
-	)
+  (
+    RESULT=$?
+    ${PHP_CS_FIXER} fix "${PHP_CF_OPTIONS[@]}" --dry-run "${DIR_NAME}" ||
+      printf " %s" "Fix by running: " "${PHP_CS_FIXER}" fix "${PHP_CF_OPTIONS[@]}" "${DIR_NAME}" $'\n'
+    exit ${RESULT}
+  )
