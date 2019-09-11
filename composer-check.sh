a9f2b567aef847700d3c88f5ab472f2d758b68f0
Jan Piskvor Martinec <github@piskvor.org>
2019-09-11 15:33:39 +0200

    validate composer well-formedness

--- composer-check.sh
+++ composer-check.sh
@@ -8,7 +8,8 @@ set -o pipefail
 
 cd "$(dirname "$0")"
 
-composer check
+composer validate --no-check-publish --no-interaction --quiet
+composer check --no-interaction
 HAS_WARNINGS="$(composer install --no-interaction --no-suggest --ansi |& tee /dev/stderr | grep -ci Warning || true)"
 if [[ "$HAS_WARNINGS" -gt 0 ]]; then
   echo "composer check failed" > /dev/stderr
