0c15b7e791fff886da02431a2ee220aa3cdd6eff
Jan Piskvor Martinec <github@piskvor.org>
2020-02-19 16:14:24 +0100

    move UcrmPluginListGenerator to own class

--- generate-json.php
+++ generate-json.php
@@ -1,5 +1,62 @@
 <?php
 
+class UcrmPluginListGenerator
+{
+    /**
+     * @var CallbackFilterIterator
+     */
+    private $pluginDirectories;
+
+    public function __construct(CallbackFilterIterator $pluginDirectories)
+    {
+        $this->pluginDirectories = $pluginDirectories;
+    }
+
+    public function getList(): array
+    {
+        $plugins = [];
+        /** @var SplFileInfo $directory */
+        foreach ($this->pluginDirectories as $directory) {
+            $file = $directory->getPathname() . '/src/manifest.json';
+
+            if (! is_readable($file)) {
+                $this->log(sprintf('Cannot access manifest "%s"' . PHP_EOL, $file));
+                continue;
+            }
+
+            $manifest = json_decode(file_get_contents($file), true, 50);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                $this->log(sprintf('Skipped file "%s": %s' . PHP_EOL, $file, json_last_error_msg()));
+                continue;
+            }
+            $plugin = $manifest['information'];
+            $plugin['name'] = $plugin['name'] ?? sprintf('(name missing in %s manifest)', $directory->getBaseName());
+            $plugin['zipUrl'] = sprintf(
+                'https://github.com/Ubiquiti-App/UCRM-plugins/raw/master/plugins/%s/%s.zip',
+                $plugin['name'],
+                $plugin['name']
+            );
+            $this->log(sprintf(
+                "\t%s\t%s\t\t%s\n",
+                $plugin['version'] ?? '(no version)',
+                $plugin['name'],
+                $plugin['url'] ?? '(URL missing)'
+            ));
+
+            $plugins[$plugin['name']] = $plugin;
+        }
+
+        ksort($plugins);
+
+        $this->log('Plugins found: ' . count($plugins) . "\n");
+        return ['plugins' => array_values(array_filter($plugins))];
+    }
+
+    private function log($text): void {
+        fwrite(STDERR, $text);
+    }
+}
+
 $pluginDirectories = new CallbackFilterIterator(
     new DirectoryIterator(__DIR__ . '/plugins'),
     static function (DirectoryIterator $fileInfo) {
@@ -7,34 +64,5 @@ $pluginDirectories = new CallbackFilterIterator(
     }
 );
 
-$plugins = [];
-/** @var SplFileInfo $directory */
-foreach ($pluginDirectories as $directory) {
-    $file = $directory->getPathname() . '/src/manifest.json';
-
-    if (! is_readable($file)) {
-        fwrite(STDERR, sprintf('Cannot access manifest "%s"' . PHP_EOL, $file));
-        continue;
-    }
-
-    $manifest = json_decode(file_get_contents($file), true, 50);
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        fwrite(STDERR, sprintf('Skipped file "%s": %s' . PHP_EOL, $file, json_last_error_msg()));
-        continue;
-    }
-    $plugin = $manifest['information'];
-    $plugin['name'] = $plugin['name'] ?? sprintf('(name missing in %s manifest)', $directory->getBaseName());
-    $plugin['zipUrl'] = sprintf(
-        'https://github.com/Ubiquiti-App/UCRM-plugins/raw/master/plugins/%s/%s.zip',
-        $plugin['name'],
-        $plugin['name']
-    );
-    fwrite(STDERR, sprintf("\t%s\t%s\t\t%s\n", $plugin['version'] ?? '(no version)', $plugin['name'], $plugin['url'] ?? '(URL missing)'));
-
-    $plugins[$plugin['name']] = $plugin;
-}
-
-ksort($plugins);
-
-fwrite(STDERR, 'Plugins found: ' . count($plugins) . "\n");
-echo json_encode(['plugins' => array_values(array_filter($plugins))], JSON_PRETTY_PRINT) . PHP_EOL;
+$listGenerator = new UcrmPluginListGenerator($pluginDirectories);
+echo json_encode($listGenerator->getList(), JSON_PRETTY_PRINT) . PHP_EOL;
