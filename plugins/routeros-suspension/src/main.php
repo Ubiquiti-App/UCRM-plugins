c0830dac474a2c04d3b519d57f8df9804efdeb6f
Martin Drlík <martin.drlik@ui.com>
2020-02-25 16:17:07 +0100

    Update plugins/routeros-suspension/src/main.php
    Co-Authored-By: Ondřej Exner <ondrej.exner@ubnt.com>

--- plugins/routeros-suspension/src/main.php
+++ plugins/routeros-suspension/src/main.php
@@ -11,7 +11,6 @@ require_once __DIR__ . '/vendor/autoload.php';
     $ipAddresses = array_map('trim', explode(PHP_EOL, $config['mikrotikIpAddress']));
     $userNames = array_map('trim', explode(PHP_EOL, $config['mikrotikUserName']));
     $passwords = array_map('trim', explode(PHP_EOL, $config['mikrotikPassword'] ?? ''));
-        : [];
 
     $configs = [];
     while ($ipAddress = array_shift($ipAddresses)) {
