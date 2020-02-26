5c3b682c3a5ecfecf04d66c933d8b66d3114f47e
Martin Drlik <martin.drlik@ui.com>
2020-02-26 09:47:14 +0100

    UCRM-4828 add log

--- plugins/routeros-suspension/src/main.php
+++ plugins/routeros-suspension/src/main.php
@@ -1,16 +1,25 @@
 <?php
 
 use Ubnt\UcrmPluginSdk\Service\PluginConfigManager;
+use Ubnt\UcrmPluginSdk\Service\PluginLogManager;
 use UcrmRouterOs\Service\Suspender;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 (static function () {
+    $log = PluginLogManager::create();
+    $log->clearLog();
+
     $config = (new PluginConfigManager())->loadConfig();
 
     $ipAddresses = array_map('trim', explode(PHP_EOL, $config['mikrotikIpAddress']));
     $userNames = array_map('trim', explode(PHP_EOL, $config['mikrotikUserName']));
     $passwords = array_map('trim', explode(PHP_EOL, $config['mikrotikPassword'] ?? ''));
+
+    if (count($ipAddresses) !== count($userNames)) {
+        $log->appendLog(sprintf('Number of rows in IP Address and User name fields does not match.'));
+        exit(1);
+    }
 
     $configs = [];
     while ($ipAddress = array_shift($ipAddresses)) {
@@ -22,6 +31,14 @@ require_once __DIR__ . '/vendor/autoload.php';
     }
 
     foreach ($configs as $config) {
-        (new Suspender($config))->suspend();
+        $error = false;
+        try {
+            $log->appendLog(sprintf('Process is starting [%s, %s]', $config['mikrotikIpAddress'], $config['mikrotikUserName']));
+            (new Suspender($config))->suspend();
+        } catch (\Exception $exception) {
+            $error = true;
+            $log->appendLog(sprintf('  - %s', $exception->getMessage()));
+        }
+        $log->appendLog(sprintf('Process is finished %s%s', $error ? '[ERROR]' : '[OK]', PHP_EOL));
     }
 })();
