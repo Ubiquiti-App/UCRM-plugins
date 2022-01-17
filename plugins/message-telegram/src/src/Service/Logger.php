4c622f79f0c21ccabdaf1d89330dba5fd689faf1
Delphi251189 <delphi251189@gmail.com>
2022-01-17 11:14:48 +0000

    added Telegram Bot API support based on sms-twilio plugin

---
+++ plugins/message-telegram/src/src/Service/Logger.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace TelegramNotifier\Service;
+
+use Psr\Log\LogLevel;
+
+class Logger extends \Katzgrau\KLogger\Logger
+{
+    private const DEFAULT_LEVEL = LogLevel::INFO; // now configurable in manifest
+    private const AVAILABLE_LEVELS = [
+     LogLevel::EMERGENCY,
+     LogLevel::ALERT,
+     LogLevel::CRITICAL,
+     LogLevel::ERROR,
+     LogLevel::WARNING,
+     LogLevel::NOTICE,
+     LogLevel::INFO,
+     LogLevel::DEBUG,
+    ];
+
+    public function __construct($level = null)
+    {
+        parent::__construct(
+            'data',
+            self::DEFAULT_LEVEL,
+            [
+                'extension' => 'log',
+                'filename' => 'plugin',
+            ]
+        );
+        if ($level) {
+            $this->setLogLevelThreshold($level);
+        }
+    }
+
+    /**
+     * @param mixed $level
+     * @param mixed $message
+     * @param array $context
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        if (!is_string($message)) {
+            $message = var_export($message,true);
+        }
+        return parent::log($level, $message, $context);
+    }
+
+    private function validateLevel($level, $defaultLevel): string
+    {
+        if (in_array($level, self::AVAILABLE_LEVELS, true)) {
+            return $level;
+        }
+        return $defaultLevel;
+    }
+
+    public function setLogLevelThreshold($logLevelThreshold): void
+    {
+        $logLevelThreshold = $this->validateLevel($logLevelThreshold, self::DEFAULT_LEVEL);
+        parent::setLogLevelThreshold($logLevelThreshold);
+        $this->notice('Logging level set to:' . $logLevelThreshold);
+    }
+}
