2023564508ebfd6459db1c34345ff25d8ea77c9c
Delphi251189 <delphi251189@gmail.com>
2022-04-12 22:53:33 +0400

    stage 3

--- plugins/message-telegram/src/hook_configure.php
+++ plugins/message-telegram/src/hook_configure.php
@@ -1,24 +1,20 @@
 <?php
-
 declare(strict_types=1);
 
-use Ubnt\UcrmPluginSdk\Exception\ConfigurationException;
-use Ubnt\UcrmPluginSdk\Security\PermissionNames;
-use Ubnt\UcrmPluginSdk\Service\UcrmApi;
-use Ubnt\UcrmPluginSdk\Service\UcrmOptionsManager;
-use Ubnt\UcrmPluginSdk\Service\PluginConfigManager;
-use Ubnt\UcrmPluginSdk\Service\UcrmSecurity;
-
 chdir(__DIR__);
-
 require_once __DIR__ . '/vendor/autoload.php';
+
+
+
+$logger = new \TelegramNotifier\Service\Logger();
+$logger->setLogLevelThreshold(LogLevel::DEBUG);
 
 $attrKey = 'telegramId';
 $attrName = 'Telegram ID';
 
-
-$api = UcrmApi::create();
+$api = Ubnt\UcrmPluginSdk\Service\UcrmApi::create();
 $customattributes = $api->get('custom-attributes');
+
 if (array_search($attrKey, array_column($customattributes, 'key')) === false)
 {
     echo '<br> ' . $attrKey . ' custom attribute not found <br>';
--- plugins/message-telegram/src/hook_disable.php
+++ plugins/message-telegram/src/hook_disable.php
@@ -1,30 +1,22 @@
 <?php
-
 declare(strict_types=1);
 
-use Ubnt\UcrmPluginSdk\Exception\ConfigurationException;
-use Ubnt\UcrmPluginSdk\Service\UcrmApi;
-use Ubnt\UcrmPluginSdk\Service\UcrmOptionsManager;
-use Ubnt\UcrmPluginSdk\Service\PluginConfigManager;
-use TelegramNotifier\Service\Logger;
-
 chdir(__DIR__);
-
 require_once __DIR__ . '/vendor/autoload.php';
 
-$pluginConfigManager = PluginConfigManager::create();
-$config = $pluginConfigManager->loadConfig();
-
-$optionsManager = UcrmOptionsManager::create();
-$options = $optionsManager->loadOPtions();
 
 $logger = new \TelegramNotifier\Service\Logger();
 $logger->setLogLevelThreshold(LogLevel::DEBUG);
+$logger->debug('Executing hook_disable');
+
+$pluginConfigManager = Ubnt\UcrmPluginSdk\Service\PluginConfigManager::create();
+$config = $pluginConfigManager->loadConfig();
+
+$optionsManager = Ubnt\UcrmPluginSdk\Service\UcrmOptionsManager::create();
+$options = $optionsManager->loadOPtions();
 
 $logger->debug('Deleting webhook ');
-
-$url = 'https://api.telegram.org/bot' . $config['telegramBotToken'] . 'deleteWebhook';
+$url = 'https://api.telegram.org/bot' . $config['telegramBotToken'] . '/deleteWebhook';
 $logger->debug($ulr);
-
 $resp = file_get_contents(url);
 $logger->debug($resp);
