d7c48a20216422ea24f4b70e1298acaf665a5a51
Jan Procházka <jan.prochazka@ui.com>
2023-06-19 10:34:43 +0200

    Quickbooks plugin update to PHP 8.1

--- plugins/quickbooks-online/src/main.php
+++ plugins/quickbooks-online/src/main.php
@@ -6,7 +6,6 @@ require 'vendor/autoload.php';
 
 (function () {
     $builder = new \DI\ContainerBuilder();
-    $builder->setDefinitionCache(new \Doctrine\Common\Cache\ApcuCache());
     $container = $builder->build();
     $plugin = $container->get(\QBExport\Plugin::class);
     try {
